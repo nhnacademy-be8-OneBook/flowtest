e41fbcb6fddf3f8fe6f039c0a05d9ebeda65eb9c
kimseonj <kimsunjun1221@gmail.com>
2024-12-03 13:11:44 +0900

    update Example

--- src/main/java/org/example/sj/Example.java
+++ src/main/java/org/example/sj/Example.java
@@ -3,5 +3,6 @@ package org.example.sj;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
